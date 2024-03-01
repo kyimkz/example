718c9f36195388cbd202dc4922c69a0ac23e9abd
kyimkz <kyimkz@gmail.com>
2024-03-01 13:35:55 +0600

    sign-up completed

--- website/userauths/views.py
+++ website/userauths/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def register_view(request):
 
     if request.method == "POST":
         form = UserRegisterForm(request.POST or None)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, Your account has been created successfully!")
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1']
+            )
+            login(request, new_user)
+            return redirect("core:index")
     else:
         form = UserRegisterForm()
 
