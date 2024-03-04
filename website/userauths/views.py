19f5ec8e780efb9087a57c3dc1468f1bef9c661e
kyimkz <kyimkz@gmail.com>
2024-03-05 03:28:38 +0600

    changes by Nazerke

--- website/userauths/views.py
+++ website/userauths/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+User = settings.AUTH_USER_MODEL
 
 def register_view(request):
 
@@ -23,3 +28,31 @@ def register_view(request):
         'form': form,
     }
     return render (request, "userauths/sign-up.html", context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect ("core:index")
+    
+    if request.method == "POST":
+        email = request.POST.get("email") 
+        password = request.POST.get("password")
+        
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exist")
+
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in.")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "User Does Not Exist, create an account.")
+
+    context = {
+
+    }
+
+    return render(request, "userauths/sign-in.html", context)
