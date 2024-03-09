5a7067b09298816aae9e9a2c25ee2833c0e55b00
tanbayevagulnara <200103501@stu.sdu.edu.kz>
2024-03-09 19:59:12 +0600

    changes from Gulya

---
+++ website/static/assets/js/sign-in.js
@@ -0,0 +1,14 @@
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerWidth - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+
+gsap.from('.general', {opacity:0, duration: 1, delay: 0.3, y:30, stagger: 0.2});  
