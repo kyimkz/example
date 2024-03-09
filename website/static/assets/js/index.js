d7eb7b87a4cd92ceac02b6c4b6cf99e6543daf49
tanbayevagulnara <200103501@stu.sdu.edu.kz>
2024-03-09 18:20:12 +0600

    new changes from Gulya

---
+++ website/static/assets/js/index.js
@@ -0,0 +1,7 @@
+gsap.from('.logo', {opacity:0, duration: 1, delay: 0.5, y:10});
+gsap.from('.first .nav-item', {opacity:0, duration: 1, delay: 0.3, y:30, stagger: 0.2});  
+gsap.from('.second .nav-item', {opacity:0, duration: 1, delay: 0.5, y:30, stagger: 0.2}); 
+
+
+gsap.from('.container', {opacity:0, duration: 1, delay: 0.5, y:10});
+gsap.from('.container .image', {opacity:0, duration: 1, delay: 0.5, y:10});
