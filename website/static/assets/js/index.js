bffd6bcae7d1c993ca3df92a0a3d80cbf2088505
tanbayevagulnara <200103501@stu.sdu.edu.kz>
2024-03-09 21:28:23 +0600

    new changes from Gulya

--- website/static/assets/js/index.js
+++ website/static/assets/js/index.js
@@ -5,3 +5,4 @@ gsap.from('.second .nav-item', {opacity:0, duration: 1, delay: 0.5, y:30, stagge
 
 gsap.from('.container', {opacity:0, duration: 1, delay: 0.5, y:10});
 gsap.from('.container .image', {opacity:0, duration: 1, delay: 0.5, y:10});
+gsap.from('.new_season', {opacity:0, duration: 1, delay: 0.3, y:30, stagger: 0.2}); 
