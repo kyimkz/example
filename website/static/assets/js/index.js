77cfb24f5d95e9f1f5228758025a055ed9088b74
kyimkz <kyimkz@gmail.com>
2024-03-12 15:33:01 +0600

    changes from Gulya

--- website/static/assets/js/index.js
+++ website/static/assets/js/index.js
@@ -6,3 +6,13 @@ gsap.from('.second .nav-item', {opacity:0, duration: 1, delay: 0.5, y:10});
 gsap.from('.container', {opacity:0, duration: 1, delay: 0.5, y:10});
 gsap.from('.container .image', {opacity:0, duration: 1, delay: 0.5, y:10});
 gsap.from('.new_season', {opacity:0, duration: 1, delay: 0.3, y:1}); 
+
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+})
