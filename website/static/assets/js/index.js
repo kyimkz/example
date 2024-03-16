b2a5cd22e00dcdde722b7c46519471f2e53f0b8d
tanbayevagulnara <200103501@stu.sdu.edu.kz>
2024-03-16 13:11:05 +0600

    changes fromm Gulya

--- website/static/assets/js/index.js
+++ website/static/assets/js/index.js
@@ -18,12 +18,3 @@ document.addEventListener('scroll', () => {
     }
 })
 
-let navbar = document.querySelector('.header .navbar')
-
-document.querySelector('#menu').onclick = () =>{
-  navbar.classList.add('active');
-}
-
-document.querySelector('#close').onclick = () =>{
-  navbar.classList.remove('active');
-}
