5e82cfbfddaa15a3d6d1d3afa1bb479b318e8f69
Aruzhan <200103330@stu.sdu.edu.kz>
2024-03-18 16:37:34 +0500

    Context proccesor, need changes in base.html

---
+++ website/core/context_processor.py
@@ -0,0 +1,6 @@
+from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+def default(request):
+    categories = Category.objects.all()
+    return {
+        'categories':categories,
+    }
