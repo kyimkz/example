c983aa5adc5f46d7737f120e2b4ceda426d6afa4
kyimkz@gmail.com <kyimkz@gmail.com>
2024-03-23 14:36:08 +0600

    category problem solved

--- website/core/context_processor.py
+++ website/core/context_processor.py
@@ -1,6 +1,8 @@
 from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+
 def default(request):
     categories = Category.objects.all()
+    
     return {
-        'categories':categories,
+        'categories': categories,
     }
